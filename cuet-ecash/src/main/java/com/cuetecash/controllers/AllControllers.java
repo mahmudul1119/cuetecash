9e7ade0236b153026b71a0060315113a122bf394
mahmudul1119 <mdmahmudulislam1119@gmail.com>
2025-09-18 21:27:01 +0600

    Create AllControllers.java

---
+++ cuet-ecash/src/main/java/com/cuetecash/controllers/AllControllers.java
@@ -0,0 +1,15 @@
+package com.cuetecash.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AllControllers {
+
+    // Root returns a simple health message for API consumers
+    @GetMapping("/")
+    public ResponseEntity<String> root() {
+        return ResponseEntity.ok("CUET eCash API is running");
+    }
+}
