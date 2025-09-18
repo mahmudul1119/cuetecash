77ac67f71a739c307bb878a18e90292cdf583c7c
mahmudul1119 <mdmahmudulislam1119@gmail.com>
2025-09-18 21:29:05 +0600

    Create PasswordHasher.java

---
+++ cuet-ecash/src/main/java/com/cuetecash/PasswordHasher.java
@@ -0,0 +1,13 @@
+package com.cuetecash;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordHasher {
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hashedPassword = encoder.encode("Officer123");
+        System.out.println(hashedPassword);
+    }
+}
+
