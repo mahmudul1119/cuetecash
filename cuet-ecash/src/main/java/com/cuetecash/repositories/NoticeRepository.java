d75a8b6cacdbc1f9f92f88f438fbe6608fbd53a6
mahmudul1119 <mdmahmudulislam1119@gmail.com>
2025-09-18 21:29:21 +0600

    Create NoticeRepository.java

---
+++ cuet-ecash/src/main/java/com/cuetecash/repositories/NoticeRepository.java
@@ -0,0 +1,9 @@
+package com.cuetecash.repositories;
+
+import com.cuetecash.models.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
